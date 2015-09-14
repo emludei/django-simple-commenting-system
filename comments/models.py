ae0492a005618b9615aca35351465ad603a4bf90
emludei <incontrol64@yandex.ru>
2015-09-14 16:45:54 +0300

    some fix

--- comments/models.py
+++ comments/models.py
@@ -1,16 +1,16 @@
 from django.conf import settings
-from django.db import models, router, transaction
+from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.postgres.fields import ArrayField
 from django.utils.translation import ugettext_lazy as _
 
-from comments.sql_operations import remove_pk_from_path_tree
+
 from comments.managers import CommentManager
 
 
-COMMENTS_MAX_LENGTH = getattr('settings', 'COMMENTS_MAX_LENGTH', 6000)
-COMMENTS_MAX_DEPTH = getattr('settings', 'COMMENTS_MAX_DEPTH', 10)
+COMMENTS_MAX_LENGTH = getattr(settings, 'COMMENTS_MAX_LENGTH', 6000)
+COMMENTS_MAX_DEPTH = getattr(settings, 'COMMENTS_MAX_DEPTH', 10)
 
 
 class Comment(models.Model):
@@ -63,23 +63,15 @@ class Comment(models.Model):
 
         if self.parent:
             tree_path.extend(self.parent.path)
-            if len(self.parent.path) < COMMENTS_MAX_DEPTH:
+            if len(tree_path) < COMMENTS_MAX_DEPTH:
+                f = open('test.txt', 'w')
+                f.write('{0}, {1}'.format(len(tree_path), COMMENTS_MAX_DEPTH))
+                f.close()
                 tree_path.append(self.id)
         else:
             tree_path.append(self.id)
 
         Comment.objects.filter(pk=self.pk).update(path=tree_path)
-
-    def delete(self, *args, **kwargs):
-        db = router.db_for_write(Comment)
-        with transaction.atomic(using=db):
-            if self.parent:
-                Comment.objects.filter(
-                    parent=self.pk,
-                ).update(parent=self.parent)
-
-            remove_pk_from_path_tree(db, self.pk, self.path)
-            super(Comment, self).delete(*args, **kwargs)
 
     def __str__(self):
         return '<Comment: id {0}, user {1}, model {2}, object_id {3}>'.format(
--- comments/sql_operations.py
+++
@@ -1,9 +0,0 @@
-from django.db import connection
-
-
-def remove_pk_from_path_tree(using, pk, path):
-    with connection[using].cursor() as cursor:
-        cursor.execute(
-            'UPDATE comments_comment SET path=array_remove(path, %s) WHERE path @> %s',
-            [pk, path]
-        )
